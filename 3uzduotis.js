b8a2c7a4133fe038f108c839c7897d11bd172ad4
velniukstea <erika.akromaite@gmail.com>
2022-06-13 15:12:53 +0300

    salyginis sakinys

---
+++ 3uzduotis.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    let x = "rutina";
+    let y = "dehidratacija";
+
+    console.log(ArDaugiauNei10Raidziu(x));
+    console.log(ArDaugiauNei10Raidziu(y));
+
+    function ArDaugiauNei10Raidziu(zodis) {
+        if (zodis.length > 10) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
